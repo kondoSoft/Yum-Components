51158f14eda1cf7dad9c5db84412ee89927e9541
Luis Juarez <skaflon_war@hotmail.com>
2017-12-12 12:10:10 -0600

    added components for restaurant view

---
+++ components/restaurantView/index.js
@@ -0,0 +1,39 @@
+import {
+  TopContainer,
+  BottomContainer,
+  BottomContainerUp,
+  BottomContainerCenter,
+  BottomContainerDown,
+  Img,
+  TitleContainer,
+  TitleText,
+  OrangeText,
+  OrangeTextContainer,
+  TextGray,
+  TextContainer,
+  LocationContainer,
+  LineContainer,
+  SocialsContainer,
+  StickContainer,
+  Touch
+} from './styled'
+
+export {
+  TopContainer,
+  BottomContainer,
+  BottomContainerUp,
+  BottomContainerCenter,
+  BottomContainerDown,
+  Img,
+  TitleContainer,
+  TitleText,
+  OrangeText,
+  OrangeTextContainer,
+  TextGray,
+  TextContainer,
+  LocationContainer,
+  LineContainer,
+  SocialsContainer,
+  StickContainer,
+  Touch
+}
